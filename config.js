0efa9221335e730c6c0dd551cd9ff32c96eb2c7e
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-06 14:25:52 +0200

    Adds winston and papertrail

--- config.js
+++ config.js
@@ -2,5 +2,8 @@
 
 module.exports = {
   DB: process.env.DB || 'mongodb://localhost/louie',
-  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go'
+  JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
+  PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-logs',
+  PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
+  PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
 }
