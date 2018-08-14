14b67c123260fdfe95a9fb777f64897e58760c53
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-08-14 07:23:37 +0200

    Updates engines (patch)

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-'use strict'
-
 module.exports = {
   DB: process.env.DB || 'mongodb://localhost/louie',
   JWT_SECRET: process.env.JWT_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
